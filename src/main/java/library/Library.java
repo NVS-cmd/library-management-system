3f490d879e04bf7fc2190baece507c13a08756e1
NVS-cmd <nagornyyyvs@student.bmstu.ru>
2025-12-27 12:53:49 +0300

    Add getStatistics method

--- src/main/java/library/Library.java
+++ src/main/java/library/Library.java
@@ -6,3 +6,16 @@ public String getStatistics() {
     return String.format("Статистика: Всего книг: %d, Доступно: %d, Выдано: %d",
             totalBooks, availableCount, issuedBooks);
 }
+public boolean removeBook(int id) {
+    Optional<Book> bookToRemove = books.stream()
+            .filter(b -> b.getId() == id)
+            .findFirst();
+
+    if (bookToRemove.isPresent()) {
+        books.remove(bookToRemove.get());
+        operationLog.addEntry(OperationLog.OperationType.ADD_BOOK,
+                "Удалена книга ID " + id + ": " + bookToRemove.get().getTitle());
+        return true;
+    }
+    return false;
+}
